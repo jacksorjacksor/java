f0e6e0ccd6219476ba2858a6dc1e4c294784e514
jacksorjacksor <jacksorjacksor@pm.me>
2022-02-10 15:59:50 +0000

    done

---
+++ three/threeOne.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+/**
+ * threeOne
+ */
+public class threeOne {
+    private float a;
+    private Date b;
+
+    public static void foobar(float a, Date b) {
+        this.a = a;
+        this.b = b;
+    }
+}
